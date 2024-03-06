9145fd456b8872a88f35d4ab9d0573512b6c0ce1
S-DiDi <157660018+jorgen24@users.noreply.github.com>
2024-03-05 22:25:29 -0500

    Update Task1.java

--- src/main/java/edu/canisius/cyb/cyb600/lab2/Task1.java
+++ src/main/java/edu/canisius/cyb/cyb600/lab2/Task1.java
@@ -1,61 +1,79 @@
 package edu.canisius.cyb.cyb600.lab2;
 
 import edu.canisius.cyb.cyb600.lab2.exceptions.NotADogException;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Task1 {
 
     /**
-     * Takes in a string and returns a List of characters (starting at 0, of course) for each character in that String.
-     * @param stringToList String to parse into characters. Empty or Null strings should be treated as empty strings.
-     * @return List of Characters. Should not return null.
+     * Converts a string to a list of characters.
+     *
+     * @param stringToList The input string to convert to a list of characters.
+     * @return A list of characters representing the input string.
      */
-    public List<Character> listAllCharacters(String stringToList){
-        if (stringToList == null){
-            return null;
+    public List<Character> listAllCharacters(String stringToList) {
+        // Return an empty list if input is null
+        if (stringToList == null) {
+            return new ArrayList<>();
         }
+
         List<Character> characterList = new ArrayList<>();
-        for(int i=1; i<stringToList.toCharArray().length;i++){
-            characterList.add(stringToList.toCharArray()[i]);
+
+        // Convert each character in the string to a list element
+        for (char c : stringToList.toCharArray()) {
+            characterList.add(c);
         }
+
         return characterList;
     }
 
     /**
-     * This method should take in two Strings. Null strings should be handled as EMPTY strings.
-     * @param baseString String that should be in the middle of the concatenation
-     * @param concat String to surround base string
-     * @return String with base surrounded by two Strings. Should not return null.
+     * Concatenates the given base string with another string at the front and end.
+     *
+     * @param baseString The base string.
+     * @param concat The string to concatenate to the front and end of the base string.
+     * @return The concatenated string.
      */
-    public String concatenateToFrontAndEnd(String baseString, String concat){
-        return baseString+concat;
+    public String concatenateToFrontAndEnd(String baseString, String concat) {
+        // Check for null values and handle accordingly
+        baseString = (baseString != null) ? baseString : "";
+        concat = (concat != null) ? concat : "";
+
+        return concat + baseString + concat;
     }
 
     /**
-     * Easy. Dog (case insensitive) goes in, you're good.
-     * Anything else but dog? That's not a dog.
-     * @param betterBeDog String that better be a dog. Null strings are not dogs.
-     * @throws NotADogException When someone is silly and doesn't put in a dog.
+     * Throws a NotADogException if the input string is not "dog" (case-insensitive).
+     *
+     * @param betterBeDog The input string to check.
+     * @throws NotADogException Thrown if the input string is not "dog".
      */
     public void throwExceptionIfNotADog(String betterBeDog) throws NotADogException {
-        if (!betterBeDog.equals("dog")) {
+        if (betterBeDog == null || !betterBeDog.equalsIgnoreCase("dog")) {
             throw new NotADogException("This isn't a dog.");
         }
     }
 
     /**
-     * Takes in a string, loops around, and returns that string 10 times in a list.
-     * @param baseString String to return. Empty strings are okay but Null strings should just return an empty list.
-     *                   We don't loop those.
-     * @return A list of 10 Strings if base string is empty or has content. Otherwise, an empty list.
+     * Returns a list containing the same string repeated ten times.
+     *
+     * @param baseString The string to be repeated.
+     * @return A list containing the repeated string.
      */
-    public List<String> returnsTheSameStringTenTimes(String baseString){
+    public List<String> returnsTheSameStringTenTimes(String baseString) {
         List<String> arrayList = new ArrayList<>();
-        while (arrayList.size()+1 != 10){
+
+        // Return an empty list if the base string is null
+        if (baseString == null) {
+            return arrayList;
+        }
+
+        // Repeat the base string and add to the list until it has 10 elements
+        while (arrayList.size() < 10) {
             arrayList.add(baseString);
         }
-        return new ArrayList<>();
+
+        return arrayList;
     }
 }
